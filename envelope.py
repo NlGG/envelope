ed4ea6915744211db27b350533aee2a1563b563a
myuuuuun <mail@myuuuuun.com>
2015-06-08 16:09:42 +0900

    no message

--- envelope.py
+++ envelope.py
@@ -5,43 +5,93 @@
 
 from __future__ import division
 import math
-import functools  #for python3
-from random import uniform, normalvariate
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
-def envelope(expression):
-    fig, ax = plt.subplots()
-    y = np.arange(0, 100, 0.5)
 
-    colormap = ['b', 'c', 'g', 'y', 'm', 'r', 'k']
+def envelope(expression, **kwargs):
 
-    for k in range(1, 101, 1):
-        color = colormap[k % 6]
-        k = k / 10.0
-        c = expression(y, k)
+    # 可変長キーワード引数（**kwargs）の初期化処理
+    x_list = kwargs.get('x_list', np.arange(0, 100, 0.5))
+    parameter_list = kwargs.get('parameter_list', np.arange(0, 10, 0.1))
+
+    title = kwargs.get('title', 'Show Envelope Curve')
+    xlabel = kwargs.get('xlabel', False)
+    ylabel = kwargs.get('ylabel', False)
+    color = kwargs.get('color', 'c')
+    legend = kwargs.get('legend', False)
+    parameter_name = kwargs.get('parameter_name', 'Parameter')
+
+    xlim = kwargs.get('xlim', [0, 100])
+    ylim = kwargs.get('ylim', [0, 30])
+    plot_size = kwargs.get('plot_size', 5)
+    
+    
+    # アニメーション関数
+    def __run(parameter):
+        y_list = expression(x_list, parameter)
         
-        min_index = c.argmin()
-        left_bound = max(min_index - 8, 0)
-        right_bound = min(min_index + 8 + 1, 200 - 1)
-        y2 = y[left_bound : right_bound]
-        c2 = c[left_bound : right_bound]
-        ax.plot(y, c, color=color, linewidth=1)
+        min_index = y_list.argmin()
+        left_bound = max(min_index - plot_size, 0)
+        right_bound = min(min_index + plot_size + 1, len(x_list) - 1)
+        x_plot_list = x_list[left_bound : right_bound]
+        y_plot_list = y_list[left_bound : right_bound]
 
-    ax.plot(0, 0, color='c', linewidth=1 ,label="Short-Run Cost Curves")
+        del plt.gca().texts[-1]
+        ax.annotate(str(parameter_name)+"="+str(parameter),
+            xy=(0.05, 0.9),
+            xycoords='axes fraction',
+            fontsize=16,
+            horizontalalignment='left',
+            verticalalignment='bottom'
+        )
 
-    plt.xlim(0, 30)
-    plt.ylim(0, 100)
-    plt.title('Long-Run Cost Curve')
-    plt.xlabel('Y: Production')
-    plt.ylabel('C: Total Cost')
-    plt.legend()
-    plt.suptitle('simulation and execution result')
+        ax.plot(x_plot_list, y_plot_list, color=color, linewidth=1)
+
+
+    # 図の初期化処理
+    fig, ax = plt.subplots()
+    ax.set_xlim(xlim[0], xlim[1])
+    ax.set_ylim(ylim[0], ylim[1])
+    ax.annotate(str(parameter_name)+"="+str(parameter_list[0]),
+        xy=(0, 0),
+        xycoords='axes fraction',
+        fontsize=16,
+        horizontalalignment='right',
+        verticalalignment='top'
+    )
+    ax.plot(0, 0, color=color, linewidth=1 ,label=str(legend))
+
+    # __runの引数にparameter_listから1つずつ値を取りながら、figにグラフを描写する
+    animation.FuncAnimation(fig, __run, parameter_list, interval=5, repeat=False)
+
+    plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+
+    if ylabel:
+        plt.ylabel(ylabel)
+
+    if legend:
+        plt.legend()
+
     plt.show()
 
 
+"""
+envelope(lambda y, k: 1/8 * (y - 10 * k) ** 2 + 1/3 * k ** 2 - 2 * k + 4,
+        plot_size = 5,
+        title = 'Show Average Long-Run Cost Curve',
+        xlabel = 'Y: Production',
+        ylabel = 'C: Cost',
+        legend = 'Average Short-Run Cost Curves',
+        parameter_name = 'K'
+        )
+"""
+
+
 envelope(lambda y, k: 1/3 * 1/50*((1/2*y-2*k**3) ** 3+(2*k**3)**3) + 1/2 * 1/5*k*((1/2*y-(5*k**3-5*k**2+k))**2-(5*k**3-5*k**2+k) **2) + 1 * 1/4 * 1/k * y + 1/100*4*k**6*y + (5*k**3-5*k**2+k) + 1/3*(k-2)**3+5*k+5)
-#envelope(lambda y, k: 1/8 * (y - 10 * k) ** 2 + 1/3 * k ** 2 - 2 * k + 4)
-#envelope(lambda y, k:1/16  * ((1/2*y-2*k**3) ** 3 + (2*k**3)**3) +1/300*5*k*((1/2*y-(8*k**3-5*k**2+5))**2-(8*k**3-5*k**2+5) **2)+5*k)
+
 
 
